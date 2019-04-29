4097b8f6eb52b1cbee998aed483996c60cfbeafe
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-29 23:59:45 +0300

    Create calculate.cpp

---
+++ calculate.cpp
@@ -0,0 +1,10 @@
+#include "calculate.hpp"
+
+using namespace std;
+using std::string;
+namespace bullpgia{
+    string bullpgia::calculateBullAndPgia(string choice, string guess){
+    string s="";
+    return s;
+}
+}
