e47d8c2931e9b1386d34a1a8313b086521a89424
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-29 23:51:04 +0300

    Update DummyGuessers.hpp

--- DummyGuessers.hpp
+++ DummyGuessers.hpp
@@ -13,6 +13,8 @@ class ConstantGuesser: public bullpgia::Guesser {
 		string guess() override {
 			return myConstantString;
 		}
+	void learn(string s) override;
+		void startNewGame(uint length) override;
 };
 
 /**
@@ -20,4 +22,6 @@ class ConstantGuesser: public bullpgia::Guesser {
  */
 class RandomGuesser: public bullpgia::Guesser {
 	string guess() override;
+	void learn(string s) override;
+	void startNewGame(uint length) override;
 };
