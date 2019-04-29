0b78e7ba980c242f122005f0387c4ab5d696752f
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-29 23:24:17 +0300

    Create SmartGuesser.cpp

---
+++ SmartGuesser.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include "SmartGuesser.hpp"
+using std::string;
+
+
+namespace bullpgia
+{
+    void SmartGuesser::learn(int reply)
+    {
+
+    }
+    string SmartGuesser::guess()
+    {
+        return "";
+    }
+    void SmartGuesser::startNewGame(uint length)
+    {
+
+    }
+};
